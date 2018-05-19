474ac4109cc2d6f58be0597d1f7afe6675d9dc8c
kesylo <keming.loic@yahoo.com>
2018-05-19 03:55:48 +0200

    19-05-18

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -25,42 +25,54 @@ public class Enemy : MonoBehaviour
 
     public Transform bulletStartPosition;
 
+    static Animator anim;
+
+    public float minDistance = 60;
+
     ParticleSystem particle;
 
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        anim = GetComponent<Animator>();
         particle = muzzleParticle.GetComponent<ParticleSystem>();
         timeBtwShots = startTimeBtwShots;
     }
 
     void Update()
     {
-        if (Vector3.Distance(transform.position, target.position) > distance)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-        }
-        else if (Vector3.Distance(transform.position, target.position) < distance && Vector3.Distance(transform.position, target.position) > retreatDistance)
-        {
-            // stop moving
-            transform.position = this.transform.position;
-        }
-        else if (Vector3.Distance(transform.position, target.position) < retreatDistance)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, target.position, -speed * Time.deltaTime);
-        }
 
-
-        if (timeBtwShots <= 0)
+        if (Vector3.Distance (target.position, this.transform.position) < 100)
         {
-            particle.Play();
-            Instantiate(EnemybulletObject, bulletStartPosition.transform.position, Quaternion.identity);  // Quaternion.identity = no rotation
-            timeBtwShots = startTimeBtwShots;
+            Vector3 direction = target.position - this.transform.position;
+            direction.y = 0;
+
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
+
+            anim.SetBool("isIdle", false);
+            if (direction.magnitude > minDistance)
+            {
+                this.transform.Translate(0, 0, 0.1f);
+                anim.SetBool("isWalkingFront", true);
+                anim.SetBool("isShooting", true);
+                Shoot();
+            }
+            else
+            {
+                anim.SetBool("isWalkingFront", false);
+                anim.SetBool("isShooting", true);
+                Shoot();
+            }
         }
         else
         {
-            timeBtwShots -= Time.deltaTime;
+            anim.SetBool("isIdle", true);
+            anim.SetBool("isWalkingFront", false);
+            anim.SetBool("isShooting", false);
         }
+
+
+        
 
     }
 
@@ -76,5 +88,19 @@ public class Enemy : MonoBehaviour
     void Die()
     {
         Destroy(gameObject);
+    }
+
+    void Shoot()
+    {
+        if (timeBtwShots <= 0)
+        {
+            particle.Play();
+            Instantiate(EnemybulletObject, bulletStartPosition.transform.position, Quaternion.identity);  // Quaternion.identity = no rotation
+            timeBtwShots = startTimeBtwShots;
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
     }
 }
